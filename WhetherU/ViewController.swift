b82e3d824b343cca15cca8197914e80326faf9fd
Coultonf <cjrfraser@gmail.com>
2016-12-13 23:02:32 -0700

    Initial Commit

---
+++ WhetherU/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhetherU
+//
+//  Created by Coulton Fraser on 2016-12-13.
+//  Copyright © 2016 CSharpShip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
